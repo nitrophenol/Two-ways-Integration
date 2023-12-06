d36a0eb9e79c0b33c97643fc782c45909126e5da
nitrophenol <mayanksingh0713@gmail.com>
2023-12-06 23:51:17 +0530

    some changes in downsyncConsumer

--- zenskar/customers/serializers/customer_serializer.py
+++
@@ -1,8 +0,0 @@
-from customers.models import Customer
-from rest_framework import serializers
-
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ('id', 'name', 'email')
